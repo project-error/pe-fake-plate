cfd54f49069e5ab4cf2e98161509b110b30716be
Darrian <darriangaylor@icloud.com>
2021-05-17 14:05:04 -0400

    Feat: Version checker

--- server/sv_utils.lua
+++ server/sv_utils.lua
@@ -46,3 +46,30 @@ AddEventHandler('onResourceStart', function(resourceName)
 		Utils.Debug('success', " " .. resourceName .. " has started.^7")
 	end
 end)
+
+CreateThread(function()
+	local updatePath = '/project-error/new-fakeplate-who-dis'
+	local resourceName = "^5["..GetCurrentResourceName().."]^2"
+
+	function checkVersion(err,responseText, headers)
+		local curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
+		if responseText == nil or curVersion == nil then
+            Utils.Debug('error', " " .. resourceName .. "^1- Update check failed. Response was nil")
+		elseif curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+            print("\n^1###############################")
+			print("\n" ..resourceName.." is outdated.")
+            print("\nThe latest stable version is "..responseText..", your version is ("..curVersion..")\n\nPlease download the latest stable build from https://github.com/project-error/new-fakeplate-who-dis")
+			print("\n^1###############################")
+		elseif(tonumber(curVersion) > tonumber(responseText)) then
+			Utils.Debug("inform","You may be using a pre-release of ^8["..GetCurrentResourceName().."]^5 or the GitHub went offline.")
+		else
+			if GetConvar('onesync') == "on" then
+				Utils.Debug("success"," ".. resourceName .. ' (v' .. responseText .. ") is up to date and has started.")
+			else
+				Utils.Debug("error"," ".. resourceName .. ' (v' .. responseText .. ")^1 is up to date but ^2[OneSync]^1 isn't enabled.\n ^1This resource requires ^2[OneSync]^1 to function correctly.")
+			end
+		end
+	end
+
+	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/dev/version", checkVersion, "GET")
+end)
