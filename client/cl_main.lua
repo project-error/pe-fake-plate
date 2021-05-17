7d243a99b44d713a0fc9e5c22e5f5c5fbbdeaff6
Ruqen <73693641+Ruqen@users.noreply.github.com>
2021-05-17 18:57:19 +0300

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,3 +1,5 @@
+ESX = exports['es_extended']:getSharedObject()
+
 local inProgress = false
 
 RegisterNetEvent('WP:setPlate', function(netId, originalPlate, fakePlate, plateType)
@@ -43,7 +45,7 @@ RegisterNetEvent('WP:setPlate', function(netId, originalPlate, fakePlate, plateT
                     if not HasAnimDictLoaded(animDict) then
                     Wait(100)
                     end
-                    TriggerEvent('WC:notify', "message", 7000, nil, "Applying plate")
+                    ESX.ShowNotification("Applying plate...")
                     TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 1.0, 1.0, 8000, 1, 0, false, false, false)
                     Wait(7500)
                     if IsEntityPlayingAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 1) then
@@ -56,12 +58,12 @@ RegisterNetEvent('WP:setPlate', function(netId, originalPlate, fakePlate, plateT
                         end
                         inProgress = false
                     else
-                        TriggerEvent('WC:notify', "error", 1000, nil, "Animation was canceled.")
+                        ESX.ShowNotification("Animation was cancelled.")
                         inProgress = false
                     end
                 end
             else
-                TriggerEvent('WC:notify', "error", 1000, nil, "You're already doing this!")
+                ESX.ShowNotification("You\'re already doing this.")
             end
         else
             print("Triggered without args")
